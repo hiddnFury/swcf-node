26566bd35db36640606a6872fae5272e086548d0
root <root@vmi2228027.contaboserver.net>
2025-03-05 20:29:47 -0600

    feat: export deob func

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -3,7 +3,6 @@ pub mod networking;
 pub mod transformers;
 pub mod utils;
 pub mod vm;
-use crate::{context::ModuleContext, handle::Handle, result::NeonResult, types::JsValue};
 
 use neon::prelude::*;
 use std::fs;
@@ -15,20 +14,25 @@ fn deobfuscate(src: &str) -> String {
     out
 }
 
-register_module!(mut cx, {
-    cx.export_function("read_file_to_string", read_file_to_string)?;
-    cx.export_function("deobfuscate_scopes", deobfuscate_scopes)?;
-});
+fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
+    Ok(cx.string("hello node"))
+}
 
 fn read_file_to_string(mut cx: FunctionContext) -> JsResult<JsString> {
     let path_prm = cx.argument::<JsString>(0)?;
-    let path_str = path_prm.value(&mut cx).to_string();
+    let path_str = path_prm.value().to_string();
     let contents = fs::read_to_string(path_str).expect("Error: Unable to read the file.");
     Ok(cx.string(contents))
 }
 
 fn deobfuscate_scopes(mut cx: FunctionContext) -> JsResult<JsString> {
     let raw_js_prm = cx.argument::<JsString>(0)?;
-    let raw_js_str = raw_js_prm.value(&mut cx).to_string();
+    let raw_js_str = raw_js_prm.value().to_string();
     Ok(cx.string(deobfuscate(&raw_js_str)))
 }
+
+register_module!(mut cx, {
+    cx.export_function("read_file_to_string", read_file_to_string)?;
+    cx.export_function("deobfuscate_scopes", deobfuscate_scopes)?;
+    cx.export_function("hello", hello)
+});
